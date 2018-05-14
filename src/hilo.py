25c85ad7dcee5886b902390cbb992a41a1c47a31
daddley <addley.david@gmail.com>
2018-05-14 17:44:16 -0600

    simple hi - low game

---
+++ src/hilo.py
@@ -0,0 +1,23 @@
+import random
+
+
+def main():
+    resp = input("Would you like to play a game?[y/n]")
+    if resp == 'y':
+        play_game()
+
+
+def play_game():
+    number = random.randint(0, 100)
+    guess = -1
+    while guess != number:
+        guess = int(input("Pick a number between 0 - 99. "))
+        if guess > number:
+            print("Too high!")
+        elif guess < number:
+            print("Too low!")
+    print("You got it!")
+
+
+if __name__ == '__main__':
+    main()
